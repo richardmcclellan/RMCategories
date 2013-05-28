480990dc6440eeeeeac7415bec2b03ca2d629a50
Richard McClellan <richard@twist.com>
2013-05-28 13:11:19 -0700

    added UIFont additions

---
+++ RMCategories/UIFont+RMAdditions.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+RMAdditions.h
+//  RMCategories
+//
+//  Created by Richard McClellan on 5/28/13.
+//  Copyright (c) 2013 Richard McClellan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (RMAdditions)
+
+-(CGFloat)lineHeight;
+
+@end
