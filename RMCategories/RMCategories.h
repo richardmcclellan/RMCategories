219724c83cd443a9a601c16f6109dc2e299a6ed6
Richard McClellan <richard@twist.com>
2013-05-28 13:22:39 -0700

    added UIFont to categories header file

--- RMCategories/RMCategories.h
+++ RMCategories/RMCategories.h
@@ -9,5 +9,6 @@
 #import "NSDictionary+RMAdditions.h"
 #import "NSMutableDictionary+RMAdditions.h"
 #import "UIApplication+RMAdditions.h"
+#import "UIFont+RMAdditions.h"
 #import "UIScrollView+RMAdditions.h"
 #import "UIView+RMAdditions.h"
